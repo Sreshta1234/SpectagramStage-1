6a6959a7070de13b3667012b41ed142ba0978bec
Sreshta1234 <104977341+Sreshta1234@users.noreply.github.com>
2022-10-19 19:11:25 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,13 @@
+import * as React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import BottomTabNavigator from './navigation/TabNavigator';
+
+
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <BottomTabNavigator/>
+    </NavigationContainer>
+  );
+}
